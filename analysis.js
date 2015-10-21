67e828ceb3b7a2190a35387479152ce6f10046b3
AlexFine <alexkfine2@gmail.com>
2015-10-21 11:41:43 -0700

    Was going to get more done but was attacked by alexis

---
+++ analysis.js
@@ -0,0 +1,29 @@
+var words = "Steven Paul Jobs (/ˈdʒɒbz/; February 24, 1955 – October 5, 2011) was an American businessman. He was best known as the co-founder, chairman, and chief executive officer (CEO) of Apple Inc.; CEO and largest shareholder of Pixar Animation Studios;[3] a member of The Walt Disney Company's board of directors following its acquisition of Pixar; and founder, chairman, and CEO of NeXT Inc. Jobs is widely recognized as a pioneer of the microcomputer revolution of the 1970s, along with Apple co-founder Steve Wozniak. Shortly after his death, Jobs's official biographer, Walter Isaacson, described him as the 'creative entrepreneur whose passion for perfection and ferocious drive revolutionized six industries: personal computers, animated movies, music, phones, tablet computing, and digital publishing.'[2]"
+
+words = words.replace("the", "");
+words = words.replace("a", "");
+words = words.replace("at", "");
+words = words.replace("who", "");
+words = words.replace("when", "");
+words = words.replace("why", "");
+words = words.replace("there", "");
+words = words.replace("when", "");
+words = words.replace("it", "");
+words = words.replace("to", "");
+words = words.replace("for", "");
+words = words.replace("the", "");
+words = words.replace("nor", "");
+words = words.replace("or", "");
+words = words.replace("but", "");
+words = words.replace("yet", "");
+words = words.replace("was", "");
+words = words.replace("he", "");
+words = words.replace("him", "");
+words = words.replace("her", "");
+words = words.replace("and", "");
+words = words.replace("of", "");
+
+
+for (int i = 0; i < words.length; i++){
+	
+}
